403c0f8d64116be9ec4c07b58a16d53facffdb90
MariaMartinezRos <cibernuggets@gmail.com>
2025-02-18 19:49:53 +0100

    Curso de Vue.js 3 - 05

--- src/components/ListaDeTareas.vue
+++ src/components/ListaDeTareas.vue
@@ -8,8 +8,9 @@
       placeholder="Escribe una nueva tarea"
     />
     <ul>
-      <li v-for="(tarea, index) in tareas" :key="index">
-        {{ tarea }}
+      <li v-for="(tarea, index) in tareas" :key="tarea.id">
+        {{ tarea.nombre }}
+        <button @click="borrarTarea(index)">Borrar</button>
       </li>
     </ul>
   </div>
@@ -18,14 +19,22 @@
 <script setup>
 import { ref } from 'vue'
 
-const tareas = ref([])
+const tareas = ref([
+  { id: 1, nombre: 'Tarea 1' },
+  { id: 2, nombre: 'Tarea 2' },
+  { id: 3, nombre: 'Tarea 3' },
+])
 const nuevaTarea = ref('')
 
 const agregarTarea = () => {
   if (nuevaTarea.value.trim() !== '') {
-    tareas.value.push(nuevaTarea.value)
+    tareas.value.push({ id: Date.now(), nombre: nuevaTarea.value })
     nuevaTarea.value = ''
   }
+}
+
+const borrarTarea = (index) => {
+  tareas.value.splice(index, 1)
 }
 </script>
 
