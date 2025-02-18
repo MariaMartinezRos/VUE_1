9fdff1369e59e30807515d8bf1a27da5529616af
MariaMartinezRos <cibernuggets@gmail.com>
2025-02-18 20:07:28 +0100

    Curso de Vue.js 3 - 06

---
+++ src/components/Registrar_1.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Formulario de Registro</h2>
+    <Form @submit="submitForm" :validation-schema="schema">
+      <div>
+        <label for="nombre">Nombre:</label>
+        <Field name="nombre" as="input" type="text" />
+        <ErrorMessage name="nombre" />
+      </div>
+      <div>
+        <label for="email">Correo:</label>
+        <Field name="email" as="input" type="email" />
+        <ErrorMessage name="email" />
+      </div>
+      <button type="submit">Registrar</button>
+    </Form>
+  </div>
+</template>
+
+<script setup>
+import { Form, Field, ErrorMessage } from 'vee-validate'
+import { schema } from '../validation-schema'
+
+const submitForm = (values) => {
+  console.log('Formulario enviado:', values)
+}
+</script>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
+import Registrar_1 from '../modules/registro/views/Registrar_1.vue'
+import ListaDeTareas from '../views/tareas/ListaDeTareas.vue'
+
+const routes = [
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView,
+  // },
+  {
+    path: '/tareas',
+    name: 'tareas',
+    component: ListaDeTareas,
+  },
+  {
+    path: '/registrar',
+    name: 'registrar',
+    component: Registrar_1,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
-  ],
+  routes,
 })
 
 export default router
